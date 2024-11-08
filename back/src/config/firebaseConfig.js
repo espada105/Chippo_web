1c4ac27ad7239fde96871b3932d5742a61b30d64
espada105 <tjddls207@naver.com>
2024-11-09 03:00:17 +0900

    [ADD] firebase

---
+++ back/src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6dkPkvRz6dwgDXgM3toj3nUGP1vZCMy8",
+  authDomain: "chippowebfirebase.firebaseapp.com",
+  projectId: "chippowebfirebase",
+  storageBucket: "chippowebfirebase.firebasestorage.app",
+  messagingSenderId: "729779844868",
+  appId: "1:729779844868:web:0f91f95222118d8bb64251",
+  measurementId: "G-R4M19WJY3Q"
+};
+
+module.exports = firebaseConfig; 
